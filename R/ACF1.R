6bff2921a107057181b54a9f17f8638e7a4f964e
niklas1347 <niklas.merz@web.de>
2021-06-22 10:18:50 +0200

    Roxygen Kommentare

--- R/ACF1.R
+++ R/ACF1.R
@@ -2,14 +2,11 @@
 set.seed(42)
 
 
-#' Dies ist die Covarianz Funktion
-#' Sie berechnet die Covarianz
-#' Die ist manchmal groß
-#' und manchmal klein
+
 covariance <- function(X, start = 1, end = length(X), h) {
   stopifnot("h ist nicht im richtigen Bereich" = (-end < h | h < end))
   stopifnot("Eingabe ist nicht numerisch" = is.numeric(X))
-  stopifnot("Eingabe ist nicht größer als eins" = end > 0)
+  stopifnot("Eingabe ist nicht groeßer als eins" = end > 0)
   stopifnot("Start- oder Endpunkt ist nicht Integer" = (start %% 1 == 0 |
                                                           end %% 1 == 0))
   n <- end - start
@@ -21,8 +18,26 @@ covariance <- function(X, start = 1, end = length(X), h) {
   }
   return((1 / n) * sum(L))
 }
-
-ACF <- function(X, start = 1, end = length(X), type, h){
+#' Dies ist die Covarianz Funktion (Title).
+#'
+#' \code{Autocovariance} berechnet die Covarianz des Eingabevektors x_1,...,x_n
+#'
+#'@param X Eingabevektor
+#'@param start Anfangszeitpunkt der Zeitreihe
+#'@param end Endzeitpunkt der Zeitreihe
+#'@param type Typ der Funktion ( Korrelation oder Kovarianz)
+#'@param h Tutor fragen
+#'
+#'@return Der Rückgabewert ist eine Abschätzung der Autokorrelationsfunktion für eine observierte Datenreihe {X_t}.
+#'
+#'@examples
+#'I <- 1000
+#'
+#'X = arima.sim(n = I, list(ar = c(0.5, 0.499), ma = c(-0.2279, 0.2488)), sd = sqrt(0.1796))
+#'
+#'Autocovariance(X, lag.max = 4, type = "correlation", plot = FALSE)
+#'@export
+Autocovariance <- function(X, start = 1, end = length(X), type, h){
   if (type == "covariance") covariance(X, start, end, h)
   if (type == "correlation") covariance(X, start, end, h)/covariance(X, start, end, 0)
 }
@@ -35,10 +50,10 @@ X = arima.sim(n = I,
               sd = sqrt(0.1796))
 plot(X, type = "l")
 
-ACF(X, type = "correlation", h = 1)
-ACF(X, type = "correlation", h = 2)
-ACF(X, type = "correlation", h = 3)
-ACF(X, type = "correlation", h = 4)
+Autocovariance(X, type = "correlation", h = 1)
+Autocovariance(X, type = "correlation", h = 2)
+Autocovariance(X, type = "correlation", h = 3)
+Autocovariance(X, type = "correlation", h = 4)
 
 acf(X, lag.max = 4, type = "correlation", plot = FALSE)
 
