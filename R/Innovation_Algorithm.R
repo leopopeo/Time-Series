c69af892fc9cbe7af618d7fb231cb0ca27ba0829
leopopeo <leograf.graf@gmail.com>
2021-06-24 18:27:19 +0200

    Struktur des Algorithmus schreiben

--- R/Innovation_Algorithm.R
+++ R/Innovation_Algorithm.R
@@ -1,2 +1,21 @@
 
 
+#Implementierung des Innovations Algorithmus
+innovations_algorithm <- function(ts,){
+  #Eingabe überprüfen
+  #Timeseries ts überprüfen
+
+  # überprüfen
+
+  # überprüfen
+
+
+  #Rekursive Berechnung der Koeffizienten Theta_n_1,..., Theta_n_n
+
+  #Berechnung des mittleren quadratischen Abweichung des ersten Elements
+
+  #Berechnung des Tetas
+
+  #Berechnung
+
+}
