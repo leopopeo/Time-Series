3f0cdf0e8ea0965f53be7beaf4e617a696903a8e
johannes97s <johannes.schnebel@web.de>
2021-06-25 17:37:53 +0200

    ARMA Generator
    Rohbau Ist jetzt fertig und funktioniert. Evtl noch kleine Spezifikationen.
    Doku muss noch erstellt werden

--- R/ARMA_Generator.R
+++ R/ARMA_Generator.R
@@ -1,95 +1,41 @@
-
 set.seed(1)
-source("AcF1.R")
 
+start = runif(3)
+phi = c(0.8, -0.3, 0.1)
+theta = c(0.8, -0.3, 0.3)
+sd = 0.2
+I = 100
 
-DLA <- function(X) {
-  end <- length(X)
-  L <- matrix(0, ncol = end, nrow = end)
-  L[1, 1] <-
-    ACF(X, lag = 1)[1] / ACF(X, lag = 0)  # Niklas: ACF(X, lag = 1)[1] hab das [1] mal dazugemacht weil sonst 2 Werte durch 1 Wert geteilt werden. hab mir die Formel aber nicht angeschaut, kann also auch falsch sein
-  v_n <- ACF(X, lag = 0)
-  for (i in 2:length(X)) {
-    z <- NULL
-    v <- v_n * (1 - L[i, i])
-    for (j in 1:(end - 1) ){
-      z <- L[end - 1, j] * ACF(X, lag = end - j)
-    }
-    z <- sum(z)
-    L[i, i] <- (ACF(X, lag= i)[i] - z) * (1 / (v))
-  }
-  v_n <<- v
-  return(L)
+reg = function(X, coeff){
+  n <- length(coeff)
+  sum(coeff*X[length(X):(length(X)-(n-1))])
 }
 
-DLA(X)
 
-#levinson(X,p=length(X))
-
-#levinson
-
-"
-gen_AR <- function(start, phi, sd, I, single = F){
-  p = length(phi)
-  X = start
-  if (single) eps = rnorm(I-p, sd = sd)
-  if (!single) eps = rep(0, I-p)
-  for (i in 1:(I-p)){
-    X <- c(X, reg(X, phi)+eps)
-  }
-  X
-}
-
-gen_AR(start, phi, 100)
-
-gen_MA <- function(theta, sd, I){
-  q <- length(theta)
-  W = rnorm(I, sd = sd )
-  Z = W[1:q]
-  for (i in 1:I){
-    Z <- c(Z, reg(Z, theta)+rnorm(n = 1, sd=sd))
-  }
-  Z[(p+1):length(Z)]
-}
-
-gen_MA(theta, 1, 100)
-"
-
-gen2 <- function(start, phi = 0, theta = 0, sd, I){
+gen <- function(start, phi = 0, theta = 0, sd, I){
   q <- length(theta)
   p = length(phi)
   X = start
-  #W = rnorm(I, sd = sd )
   Z = rnorm(q, sd=sd)
   for (i in 1:I){
-    Z <- c(Z, reg(Z, theta)+rnorm(n = 1, sd=sd))
-    X <- c(X, reg(X, phi) + Z[i+q])
+    Z <- c(Z, rnorm(n = 1, sd=sd))
+    X <- c(X, reg(X, phi) + reg(Z[-length(Z)], theta) + Z[i+q])
   }
   X[(length(start)+1):length(X)]
 }
-
-Y1 = gen2(start, phi, theta, sd, I)
+#start evtl nicht als Argument sondern als Zufallsvektor in Fkt erstellen
+#
+Y1 = gen(start, phi, theta, sd, I)
 plot(Y1, type ="l")
 
 Y2 = arima.sim(n = I,
                list(ar = phi,
                     ma = theta,
                     sd = sd))
-plot(Y2, type = "l")
+#plot(Y2, type = "l")
 
 arima(Y1, order = c(length(phi), 0, length(theta)))
 arima(Y2, order = c(length(phi), 0, length(theta)))
 c(phi, theta)
 
 
-gen <- function(start, phi = 0, theta = 0, sd, I){
-  if (identical(phi, 0)){
-    return(gen_MA(theta, sd, I))
-  }
-  if (identical(theta, 0)){
-    return(gen_MA())
-  }
-}
-
-T1 = gen2(start, sd, phi, theta, I)
-plot(T1, type = "l" )
