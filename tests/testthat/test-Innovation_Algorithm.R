15d24c9bc2eb2b2d476d9bf89671f870338feca3
leopopeo <leograf.graf@gmail.com>
2021-06-29 10:58:50 +0200

    Update test-Innovation_Algorithm.R

--- tests/testthat/test-Innovation_Algorithm.R
+++ tests/testthat/test-Innovation_Algorithm.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
+test_that("Ueberpruefung der Eingabewerte", {
+  expect_error(innovation(NA),"Was erwartet wird")
+})
+
+test_that("Innovation Algorithmus funktioniert", {
   expect_equal(2 * 2, 4)
 })
