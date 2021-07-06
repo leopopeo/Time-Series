edb4e671340975cdee075f719bd314416b69c690
johannes97s <johannes.schnebel@web.de>
2021-07-06 19:12:02 +0200

    Es tut
    Ist aber noch sehr langsam

--- R/Prediction_Innovation.R
+++ R/Prediction_Innovation.R
@@ -14,10 +14,10 @@ estimate <- function(X, n){
 }
 
 X = arima.sim(n = 100, list(
-  ar = c(0.8897, 0.0858),
-  ma = c(-0.002279, 0.002488)),
+  ar = c(0.95),
+  ma = c(0.7, 0.25)),
   sd = sqrt(0.1796))
-
+plot(X)
 X_hat = estimate(X, 50)
 plot(X)
 lines(X_hat, col = "red")
@@ -31,34 +31,38 @@ ts_predict <- function(X, steps){
   }
   X_hat
 }
+
+ts_predict(X, 1)
 X_test <- X[1:80]
-X_est = ts_predict(X_test, 10)
+X_est = ts_predict(X_test, 1)
 plot(X_est, type = "l", col = "red")
 lines(X)
+
+
 ########################
 #Determinate Theta
-lag.max = 10
+lag.max = length(X)-1
 steps = 1
 inno_result <- innovation(X,lag.max+steps-1)
 theta <- inno_result
 
 
 #Sum need to Prediction of previous Elements
-next_x_sum <- function(theta,x,x_next,i){
+next_x_sum <- function(theta,X,x_next,i){
   theta_ <- theta[i,i:1]
-  x_cache <- x[2:(i+1)]
+  x_cache <- X[2:(i+1)]
   sum(theta_*(x_cache-x_next))
 }
 x_next <- 0 # First Prediction
 #Calculate previous Elements predictionssf
 for (i in seq(steps+lag.max-2)){
-  x_next[i+1] <- next_x_sum(theta,x,x_next,i)
+  x_next[i+1] <- next_x_sum(theta,X,x_next,i)
 }
 #Calculate lag.max+steps+1 Element prediction
 theta_ <- theta[lag.max+steps-1,steps:(lag.max+steps-1)]
-x_ <-x[(lag.max+steps):(steps+1)]
+x_ <-X[(lag.max+steps):(steps+1)]
 x_next_ <- x_next[(lag.max+steps-1):steps]
 x_prediction <- sum(theta_*(x_-x_next_))
 
 x_prediction
-}
+
