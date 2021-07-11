2330b54be8b30b632010df8cc197b899859e21b8
niklas1347 <niklas.merz@web.de>
2021-07-11 15:12:44 +0200

    Test-ARMA Eingabe werte überprüfen

--- tests/testthat/test-ARMA_Generator.R
+++ tests/testthat/test-ARMA_Generator.R
@@ -1,5 +1,19 @@
 context("Test of the implementation ’ARAM-Generator'")
 
+xtest_that("Test der Eingabewerte", {
+  start = runif(3)
+  sd = 0.2
+  I = 100
+  # Phi & Theta
+  expect_error(gen(start,phi=c("b"),sd,I), "phi und theta muss ein numerischer Vektor oder NULL sein")
+  expect_error(gen(start,theta=c("b"),sd,I), "phi und theta muss ein numerischer Vektor oder NULL sein!")
+
+  # sd
+
+  #I
+
+})
+
 test_that("AR works", {
 print("Test")
 })
