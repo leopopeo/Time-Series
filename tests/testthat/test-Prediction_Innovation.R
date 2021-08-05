93429be6f67bc73f9c92c5ad34d505d18190a598
niklas1347 <niklas.merz@web.de>
2021-08-05 17:11:40 +0200

    Update

--- tests/testthat/test-Prediction_Innovation.R
+++ tests/testthat/test-Prediction_Innovation.R
@@ -4,10 +4,10 @@ test_that("Eingabe testen", {
   expect_error(ts_predict(NULL), "Eingabe ist nicht numerisch!")
   expect_error(ts_predict(c(1)), "Die Länge des Vektors muss größer als 1 sei!")
 
-  expect_error(ts_predict(c(3,4,5,1), steps=1.1), "len muss >= 2 sein!")
-  expect_error(ts_predict(c(3,4,5,1), steps=c(1,1)), "len muss >= 2 sein!")
-  expect_error(ts_predict(c(3,4,5,1), steps=NA), "len muss NULL oder ein Integer Wert sein!")
-  expect_error(ts_predict(c(3,4,5,1), steps="a"), "len muss NULL oder ein Integer Wert sein!")
+  expect_error(ts_predict(c(3,4,5,1), steps=1.1), "len muss NULL oder ein Integer Wert sein!")
+  expect_error(ts_predict(c(3,4,5,1), steps=c(1,1)), "len muss NULL oder ein Integer Wert sein!")
+  expect_error(ts_predict(c(3,4,5,1), steps=NA), "len muss >= 1 sein!")
+
 
   expect_error(ts_predict(c(3,4,5,1), steps=2, all= c("a","b"),"Eingabe ist kein logical Wert"))
   expect_error(ts_predict(c(3,4,5,1), steps=2, all= 1.1,"Eingabe ist kein logical Wert"))
